8ceee582c3f5560b1b3a7fe9e0af00c2bad08d8f
Ian Davies <idda@amazon.com>
2021-02-03 14:40:10 -0800

    Add recent vs current tabs

--- src/StatusBar.tsx
+++ src/StatusBar.tsx
@@ -1,8 +1,12 @@
 import * as React from "react";
 import {QueryStats} from "./session";
 import Select from "react-select";
+import {Tab, Tabs} from "@material-ui/core";
+import HistoryIcon from '@material-ui/icons/History';
+import ReceiptIcon from '@material-ui/icons/Receipt';
 
 export default({ ledgers, ledger, setLedger, queryStats }: { ledgers: string[], ledger: string, setLedger: (ledger: string) => void, queryStats?: QueryStats}) => {
+    const [selectedTab, setSelectedTab] = React.useState("current");
     return <div className="status-bar">
         <span className="status-item" style={{ width: "300px" }}>
             <Select
@@ -19,5 +23,19 @@ export default({ ledgers, ledger, setLedger, queryStats }: { ledgers: string[], 
         {queryStats
             ? <span className="status-item">Read IOs: {queryStats.consumedIOs.getReadIOs()}; Time: {queryStats.timingInformation.getProcessingTimeMilliseconds()}ms</span>
             : <></>}
+        <span className="status-item" style={{ flex: 1 }}>
+            <Tabs
+                indicatorColor="primary"
+                textColor="primary"
+                value={selectedTab}
+                defaultValue="current"
+                onChange={(e, value) => {
+                    setSelectedTab(value);
+                }}
+            >
+            <Tab icon={<ReceiptIcon />} value="current" />
+            <Tab icon={<HistoryIcon />} value="history" />
+          </Tabs>
+        </span>
     </div>;
 }
