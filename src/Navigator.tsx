117f507e03d62507ded4d58ce8fce9d4a9112b95
Johan Jansen van Rensburg <jjrensbu@amazon.com>
2021-02-03 16:12:39 -0800

    Basic functionality to fetch the tables in a ledger

--- src/Navigator.tsx
+++ src/Navigator.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { makeStyles } from '@material-ui/core/styles';
+import { getLedgerMetaData } from "./ledger";
 import TreeView from '@material-ui/lab/TreeView';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
@@ -8,26 +9,28 @@ import TreeItem from '@material-ui/lab/TreeItem';
 
 const useStyles = makeStyles({
     root: {
-      height: 240,
-      flexGrow: 1,
-      maxWidth: 400,
+        height: 240,
+        flexGrow: 1,
+        maxWidth: 400,
     },
 });
 
-export default ({ ledgers } : { ledgers: string[] }) => {
+export default ({ ledgers }: { ledgers: string[] }) => {
     const classes = useStyles();
 
-        return (
-          <TreeView
+    return (
+        <TreeView
             className={classes.root}
             defaultCollapseIcon={<ExpandMoreIcon />}
             defaultExpandIcon={<ChevronRightIcon />}
-          >
-            { ledgers.map((ledger) => {
-                return <TreeItem key={"ledger-" + ledger} nodeId={"ledger-" + ledger} label={ledger}>
-                    <TreeItem key={"ledger-" + ledger + "-tables"} nodeId={"ledger-" + ledger + "-tables"} label="Tables" />
+        >
+            { ledgers.map((ledger, index) => {
+                getLedgerMetaData(ledger).then(l => { console.log(l) })
+                const key = index.toString() + ledger
+                return <TreeItem key={key} nodeId={key} label={ledger}>
+                    <TreeItem key={"t" + key} nodeId={"t" + key} label="Tables" />
                 </TreeItem>
             })}
-          </TreeView>
-        );
-      };
+        </TreeView>
+    );
+};
--- src/ledger.ts
+++ src/ledger.ts
@@ -1,4 +1,5 @@
-import {QLDB} from "aws-sdk";
+import { QLDB } from "aws-sdk";
+import { openLedger } from "./session";
 
 /**
  * Create a ledger and wait for it to be active.
@@ -7,4 +8,13 @@ import {QLDB} from "aws-sdk";
 export async function listLedgers() {
     const ledgers = await new QLDB().listLedgers().promise();
     return ledgers.Ledgers.filter(l => l.State === "ACTIVE").map(l => l.Name);
+}
+
+export async function getLedgerMetaData(ledger: string) {
+    const result = await openLedger(ledger).execute("SELECT * FROM information_schema.user_tables")
+    const tables = JSON.parse(JSON.stringify(result.getResultList()));
+    return {
+        name: ledger,
+        tables: tables
+    }
 }
