1628a6ef66de2bc1e0d248a8d9c508ca5d76e51d
Gautam Anghore <anghoreg@amazon.com>
2021-02-04 11:47:02 -0800

    Make basic table view in results work

--- src/Results.tsx
+++ src/Results.tsx
@@ -6,9 +6,20 @@ import AccordionSummary from '@material-ui/core/AccordionSummary';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import {BottomNavigation, BottomNavigationAction, Fab, Paper} from "@material-ui/core";
+import {
+    BottomNavigation,
+    BottomNavigationAction,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow
+} from "@material-ui/core";
 import TableChartIcon from '@material-ui/icons/TableChart';
 import CodeIcon from '@material-ui/icons/Code';
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -26,45 +37,25 @@ const useStyles = makeStyles((theme) => ({
     navigation: {
         width: 200,
         backgroundColor: Color.DARKGRAY
+    },
+    table: {
+        minWidth: 650,
+    },
+    tableHeadContent: {
+        fontWeight: theme.typography.fontWeightBold
     }
 }));
 
 export default ({ resultsText }: { resultsText: string }) => {
     const classes = useStyles();
     const [viewType, setViewType] = React.useState("ion");
-
     const queryResult = resultsText ? JSON.parse(resultsText) : []
-    const items = []
-    for (const i in queryResult) {
-        items.push(
-            <Accordion key={"result-" + i}>
-                <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls={"result-panel-content-" + i} id={"result-panel-header-" + i}>
-                    <div style={{overflow: "hidden", textOverflow: "ellipsis", maxWidth: "35em"}}>
-                        <Typography className={classes.headingAccordion} noWrap>{JSON.stringify(queryResult[i], undefined)}</Typography>
-                    </div>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <pre>
-                        { JSON.stringify(queryResult[i], undefined, 2) }
-                    </pre>
-                </AccordionDetails>
-            </Accordion>
-        )
-    }
 
-    const ionView = <div style={{maxHeight: 300, overflow: "scroll"}}>
-        { items }
-    </div>
-
-    const tableView = <div style={{maxHeight: 300, overflow: "scroll"}}>
-        { resultsText }
-    </div>
-
-    var result
+    let resultView
     if (viewType == "table") {
-        result = tableView
+        resultView = prepareTableView(queryResult)
     } else {
-        result = ionView
+        resultView = prepareIonView(queryResult)
     }
 
     return <Paper style={{backgroundColor: Color.DARKGRAY, flex: 1, width: "100%", height: "100%"}}>
@@ -83,7 +74,74 @@ export default ({ resultsText }: { resultsText: string }) => {
             </BottomNavigation>
             <Typography className={classes.heading} style={{flex: 1}}>Results</Typography>
         </div>
-        { result }
+        { resultView }
 
     </Paper>
+}
+
+function prepareIonView(queryResult: []) {
+    const classes = useStyles();
+    const items = []
+    for (const i in queryResult) {
+        items.push(
+            <Accordion key={"result-" + i}>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls={"result-panel-content-" + i} id={"result-panel-header-" + i}>
+                    <div style={{overflow: "hidden", textOverflow: "ellipsis", maxWidth: "35em"}}>
+                        <Typography className={classes.headingAccordion} noWrap>{JSON.stringify(queryResult[i], undefined)}</Typography>
+                    </div>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <pre>
+                        { JSON.stringify(queryResult[i], undefined, 2) }
+                    </pre>
+                </AccordionDetails>
+            </Accordion>
+        )
+    }
+    return <div style={{maxHeight: 300, overflow: "scroll"}}>
+        { items }
+    </div>
+}
+
+function prepareTableView(queryResult: []) {
+    const classes = useStyles();
+    const headers = findAllHeaders(queryResult)
+
+    return <div style={{maxHeight: 300, overflow: "scroll"}}>
+        <TableContainer component={Paper}>
+            <Table className={classes.table} size="small" aria-label="Results table">
+                <TableHead>
+                    <TableRow>
+                        {headers.map((header) => <TableCell>
+                            <Typography className={classes.tableHeadContent}>{header}</Typography>
+                        </TableCell>)}
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {queryResult.map((result) => prepareTableContentRow(result, headers))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    </div>
+
+}
+
+function prepareTableContentRow(queryResult: {}, headers: []) {
+    return <TableRow>
+        {headers.map((header) => (
+            <TableCell>{JSON.stringify(queryResult[header])}</TableCell>
+        ))}
+    </TableRow>
+}
+
+function findAllHeaders(queryResult: []): [] {
+    const headers: Map<string, number> = new Map<string, number>()
+    for (const i in queryResult) {
+        const map: Map<string, string> = queryResult[i]
+        for (const key in map) {
+            const count = headers.get(key) ? headers.get(key) + 1 : 1
+            headers.set(key, count)
+        }
+    }
+    return Array.from(headers.entries()).sort((a, b) => b[1] - a[1]).map((e) => e[0]) as []
 }
