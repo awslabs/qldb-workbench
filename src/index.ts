8ce9a2bc3d5c8c7711a2f46d59504cbd70bcc592
Ian Davies <idda@amazon.com>
2021-06-23 09:52:33 -0700

    Electron crashes when trying to do reloads, this fixes that

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,23 @@ import { app, BrowserWindow } from "electron";
 import * as electronIsDev from "electron-is-dev";
 import * as path from "path";
 
+let myWindow: BrowserWindow | null = null;
+
+if (electronIsDev.valueOf()) {
+  console.log("Electron started in development mode");
+  console.log("__dirname:", __dirname);
+  console.log("process.platform:", process.platform);
+}
+
 require("electron-reload")(__dirname, {
-  electron: path.join(
-    __dirname,
-    "..",
-    "..",
-    "node_modules",
-    ".bin",
-    "electron"
-  ),
+  electron: undefined // this is deliberately not set because it causes SIGABRTs on MacOS
 });
 
 function createWindow() {
+  if (myWindow) {
+    return myWindow;
+  }
+
   const win = new BrowserWindow({
     width: 1680,
     height: 1050,
@@ -28,18 +33,38 @@ function createWindow() {
       win.webContents.toggleDevTools();
     }
   });
+
+  return win;
 }
 
-app.whenReady().then(createWindow);
+const gotTheLock = app.requestSingleInstanceLock()
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
-});
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    // Someone tried to run a second instance, we should focus our window.
+    if (myWindow) {
+      if (myWindow!.isMinimized()) myWindow!.restore()
+      myWindow.focus()
+    }
+  })
 
-app.on("activate", () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
+  // Create myWindow, load the rest of the app, etc...
+  app.whenReady().then(() => {
+    myWindow = createWindow()
+  });
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      myWindow = createWindow();
+    }
+  });
+
+  app.on("window-all-closed", function () {
+    if (process.platform !== "darwin") {
+      app.quit();
+    }
+  });
+
+}
